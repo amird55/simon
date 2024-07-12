327e7b00020cbfc768230ff5f53a7c9e9746178e
amird55 <amir.dgn@outlook.com>
2024-07-12 13:10:44 +0300

    גרסה 0 - הדלקת נורות ברצף לפי סדר קבוע

--- simon.ino
+++ simon.ino
@@ -1,9 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
+#define pinLed_G 6
+#define pinLed_Y 9
+#define pinLed_B 10
+#define pinLed_R 11
 
+#define NUM_OF_LEDS 4
+int Leds[NUM_OF_LEDS]={pinLed_G,pinLed_Y,pinLed_B,pinLed_R};
+
+void setup() {
+  for(int k=0;k<NUM_OF_LEDS; k++){
+    pinMode(Leds[k],OUTPUT);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  for(int k=0;k<NUM_OF_LEDS; k++){
+    LedBlink(k);
+  }
 }
+void LedBlink(int chnl){
+  digitalWrite(Leds[chnl],HIGH);
+  delay(600);
+  digitalWrite(Leds[chnl],LOW);
+}
+
+
+
+
+
+
