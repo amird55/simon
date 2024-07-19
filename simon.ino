b92e4474f32592cabb3fb6c70a4273fafcf3c4ec
amird55 <amir.dgn@outlook.com>
2024-07-19 12:22:47 +0300

    הוספת צלילים

--- simon.ino
+++ simon.ino
@@ -1,3 +1,7 @@
+#include "pitches.h"
+
+#define pinPsvBzzr 5
+
 #define pinLed_G 6
 #define pinLed_Y 9
 #define pinLed_B 10
@@ -5,11 +9,13 @@
 
 #define NUM_OF_LEDS 4
 int Leds[NUM_OF_LEDS]={pinLed_G,pinLed_Y,pinLed_B,pinLed_R};
+int Tones[NUM_OF_LEDS]={NOTE_C4, NOTE_G3, NOTE_A3, NOTE_F2};
 #define MAX_NUM_OF_LIGHTS 10
 int ChosenIndexes[MAX_NUM_OF_LIGHTS];
 int NumOfChosenIndexes=0;
 
 void setup() {
+  pinMode(pinPsvBzzr, OUTPUT);
   randomSeed(analogRead(A1));
   for(int k=0;k<NUM_OF_LEDS; k++){
     pinMode(Leds[k],OUTPUT);
@@ -36,7 +42,8 @@ void loop() {
 }
 void LedBlink(int chnl){
   digitalWrite(Leds[chnl],HIGH);
-  delay(600);
+  tone(pinPsvBzzr,Tones[chnl],600);
+  //delay(600);
   digitalWrite(Leds[chnl],LOW);
 }
 
